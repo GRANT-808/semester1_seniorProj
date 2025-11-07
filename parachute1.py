45a9d67f9a06b6b303b89898fe7aeeb600720b71
Dennis Evangelista <devangel77b@gmail.com>
2025-11-06 20:45:23 -0500

    blah

--- parachute1.py
+++ parachute1.py
@@ -1,11 +1,21 @@
 import svgwrite
 import numpy as np
 
+
+Dmin = 1
+delta = 0.25
+Nring = 5
+Nsegment = 4
+
 print('open the file')
 dwg = svgwrite.Drawing('kirichute1.svg')
 
-for ring in range(5):
-    for segment in range(4):
+for ring in range(Nring):
+    R = Dmin+ring*delta
+    for segment in range(Nsegment):
+        theta0 = 0 + 2*np.pi/Nsegment*segment
+        thetaf = 0 + 2*np.pi/Nsegment*(segment+1)-delta/R
         print('draw a segment here LATER')
 
+print('draw the circle around the outside last')
 print('save the file')
